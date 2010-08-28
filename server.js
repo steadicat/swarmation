4dedd55a63d155d64f7c6b288ec3f632fa8faa9b
Zach Bialecki <zach.bialecki@gmail.com>
2010-08-28 15:57:28 -0400

    Added the player object and updated the bindings

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.configure(function(){
     app.set('views', __dirname + '/views');
     app.use(connect.bodyDecoder());
     app.use(connect.methodOverride());
-    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
     app.use(app.router);
     app.use(connect.staticProvider(__dirname + '/public'));
 });
