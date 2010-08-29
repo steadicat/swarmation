443f3014498d6f0d52455caaadbcb682ddf84485
Stefano J. Attardi <stefano@attardi.org>
2010-08-29 12:06:08 -0400

    Added player names and scores, some networking/event cleanup.

--- public/js/page.js
+++ public/js/page.js
@@ -1,12 +1,23 @@
-function displayNotice(text) {
-	var div = $("<p />")
-		.hide()
-		.text(text)
-		.addClass("sidebar-notice")
-		.css({ "opacity": "0.0", "color": "#FFC0CB" })
-		.prependTo($("#sidebar"))
-		.slideDown()
-		.animate({ "opacity": "1.0" }, "slow");
+(function($, undefined) {
 
-	setTimeout(function(){div.css({ "color": "#fff" });}, 1000);	
-}
+    function displayNotice(text) {
+        var div = $("<p />")
+            .hide()
+            .text(text)
+            .addClass("sidebar-notice")
+            .css({ "opacity": "0.0", "color": "#FFC0CB" })
+            .prependTo($("#sidebar"))
+            .slideDown()
+            .animate({ "opacity": "1.0" }, "slow");
+
+        setTimeout(function(){div.css({ "color": "#fff" });}, 1000);
+    }
+
+
+    $(document).bind('keydown', 'up', function() { return false; });
+    $(document).bind('keydown', 'down', function() { return false; });
+    $(document).bind('keydown', 'left', function() { return false; });
+    $(document).bind('keydown', 'right', function() { return false; });
+    $(document).bind('keydown', 'space', function() { return false; });
+
+})(jQuery);
