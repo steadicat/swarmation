f4b5ba429da0e3de6b4ff0d4470f7a9c292dea51
Stefano J. Attardi <stefano@attardi.org>
2010-08-29 14:20:07 -0400

    Formations are now indexed by name.

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,32 +1,32 @@
-var Formations = [
-    {
+var Formations = {
+    'Easy': {
         points: [[1,0], [-1,0]],
         name: 'Easy',
         reward: '',
         showOutline: true
     },
-    {
+    'Apple Key': {
         points: [[-1,-1], [1,-1], [-1,1], [1,1]],
         name: 'Apple Key',
         reward: '',
         showOutline: true
     },
-    {
+    'Tetris': {
         points: [[0,1], [0,2], [0,3], [0,4]],
         name: 'Tetris',
         reward: '',
         showOutline: true
-    },  
-    {
+    },
+    'Delta': {
         points: [[-2,2], [-1,1], [1,1], [2,2]],
         name: 'Delta',
         reward: '',
         showOutline: true
-    },  
-    {
+    },
+    'The Tank': {
         points: [[-1,-1], [-1,-2], [0,-3], [1,-2], [1,-1]],
         name: 'The Tank',
         reward: '',
         showOutline: true
     }
-];
+};
