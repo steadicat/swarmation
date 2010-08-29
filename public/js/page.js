fba53b5f973d5fd0ae67213d7c66e292e66f4549
David <ollerac@storylog.com>
2010-08-29 13:22:28 -0700

    input box for a user name

--- public/js/page.js
+++ public/js/page.js
@@ -15,6 +15,42 @@ var displayNotice;
         setTimeout(function(){div.css({ "color": "#fff" });}, 1000);
     };
 
+	 function setupPlaceholder(inputid) {
+     if ($.browser.webkit) return false;
+		 
+     var target = $('#'+inputid);
+     if (target.length==0) {
+       target = $('input[type="text"], input[type="email"], input[type="search"]');
+     }
+		 
+     target.each( function(i, el) {
+										el = $(el);
+										var ph = el.attr('placeholder');
+										if (!ph) return true;
+										
+										el.addClass('placeholder');
+										el.attr('value', ph);
+										
+										el.focus( function(e) {
+																if( el.val()==ph ) {
+																	el.removeClass('placeholder');
+																	el.attr('value', '');
+																}
+															});
+										
+										el.blur( function(e) {
+															 if( $.trim(el.val())=='' ) {
+																 el.addClass('placeholder');
+																 el.attr('value', ph);
+															 }
+														 });
+									});
+	 }
+
+	 setupPlaceholder("username");
+
+    $("#username");
+
     $(document).bind('keydown', 'up', function() { return false; });
     $(document).bind('keydown', 'down', function() { return false; });
     $(document).bind('keydown', 'left', function() { return false; });
