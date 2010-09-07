ffb728dd004824529a29054f79babbaf6008df55
Stefano J. Attardi <stefano@attardi.org>
2010-09-07 14:24:14 -0700

    Changing server restart signal not to interfere with regular server restarts.

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ process.on('uncaughtException', function(e) {
     sys.log(e.stack);
 });
 
-process.on('SIGTERM', function() {
+process.on('SIGINT', function() {
     if (socket) socket.broadcast({ type: 'restart' });
     setTimeout(function() {
         process.exit();
