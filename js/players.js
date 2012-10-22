baf86a500e6c9a72b5bcc31f53adafcbad8f5103
Stefano J. Attardi <attardi@fb.com>
2012-10-21 21:59:26 -0700

    Fix send button

--- js/players.js
+++ js/players.js
@@ -397,16 +397,15 @@ var requestPopup
 
 function showRequestPopup() {
   if (requestPopupShown) return
-  var button = Html.div('.fb-send', { 'data-href': 'http://swarmation.com' })
+  var button = Dom.get('send')
+  Dom.remove(button)
+  Dom.removeClass(button, 'off')
   requestPopup = Html.div('.megaphone.pvs', [
     'Swarmation is more fun with more people. Ask some friends to join: ',
     button
   ])
   Dom.get('container').appendChild(requestPopup)
   requestPopupShown = true
-  button.addEventListener('click', function() {
-    if (requestPopup) Dom.remove(requestPopup)
-  })
 }
 
 var RESTARTING = false
