71c7cdd487470d12d77fa8029b07ab10aa192bfb
Stefano J. Attardi <stefano@attardi.org>
2020-04-25 14:53:24 -0700

    Node 12 on server

--- etc/setup.sh
+++ etc/setup.sh
@@ -9,7 +9,7 @@ replace() {
 add-apt-repository ppa:certbot/certbot
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 apt-get update -y
 apt-get upgrade -y
